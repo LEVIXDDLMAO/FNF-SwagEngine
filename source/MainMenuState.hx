9e021e4595a19028e77d817245f0b222de2f5212
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-24 00:37:52 -0600

    E

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -113,7 +113,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
+			menuItem.x = 300;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
