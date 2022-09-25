2fd20889b9c103369a4b028a47478ceecbe0bc66
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-24 23:39:49 -0600

    oo

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1154,8 +1154,8 @@ class PlayState extends MusicBeatState
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 
-				case 'tutorial'
-				    startDialogue(dialogueJson):
+				case 'tutorial':
+				    startDialogue(dialogueJson);
 
 				default:
 					startCountdown();
