8acb7c92291270bae9c884bc64587b5c382a9ec2
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-25 01:02:05 -0600

    added it back since i can

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1154,6 +1154,10 @@ class PlayState extends MusicBeatState
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 
+				case 'tutorial':
+				    startDialogue(dialogueJson);
+	
+
 				default:
 					startCountdown();
 			}
