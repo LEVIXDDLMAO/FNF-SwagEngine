9e0337f3aec3edd7b63ad6b806ad1669cfe83843
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-26 22:42:12 -0600

    moved to ver 0.3.5  ( since updates are droped fast

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var swagengineVersion:String = '0.2.5'; //This is also used for Discord RPC
+	public static var swagengineVersion:String = '0.3.5'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
