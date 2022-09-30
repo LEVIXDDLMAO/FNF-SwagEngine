f4b29a2434501ca239e0c8d82a3c985c59ac7bf5
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-29 22:47:25 -0600

    ugh

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,7 +35,7 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		'mods', 
+		#if MODS_ALLOWED 'mods', #end
 		'awards',
 		'credits',
 		'options',
@@ -233,8 +233,10 @@ class MainMenuState extends MusicBeatState
 										MusicBeatState.switchState(new StoryMenuState());
 									case 'freeplay':
 										MusicBeatState.switchState(new FreeplayState());
+									#if MODS_ALLOWED
 									case 'mods':
 										MusicBeatState.switchState(new ModsMenuState());
+									#end;
 									case 'awards':
 										MusicBeatState.switchState(new AchievementsMenuState());
 									case 'credits':
